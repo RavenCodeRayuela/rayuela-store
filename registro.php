a3c6a57bbb6f36fdc8a905d344d7b3e57aac9d51
Juan Letamendia <juanletamendia22@gmail.com>
2024-07-31 02:19:54 -0300

    KAN-3 Inicio de formulario de registro.

--- registro.php
+++ registro.php
@@ -1,2 +1,17 @@
-<?php 
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //Para validar que los campos sean correctos y crear el nombre de usuario.
+    if(!empty($_POST['nombre']) && !empty($_POST['apellido']) ){
+        $nombre= $_POST['nombre'];
+        $apellido= $_POST['apellido'];
+
+        $nombre= filter_var($nombre, FILTER_SANITIZE_STRING);
+        $apellido= filter_var($apellido, FILTER_SANITIZE_STRING);
+        $nombreDeUsuario = $nombre." ".$apellido;
+
+        echo $nombreDeUsuario;
+    }
+}
+
 ?>
